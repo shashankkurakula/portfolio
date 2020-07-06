c3eaaa2e8871d62f4d599c150180ff1bc1b136ba
shashankkurakula <shashankkurakula96@gmail.com>
2020-07-06 23:08:02 +0530

    Updates

---
+++ precache-manifest.83b17c48bf7190c3e563f4b8bdfd5844.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22e8df752347722d0b4ab175b6eac613",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "cb4eed8b429102fa263c",
+    "url": "/portfolio/static/css/main.a9a3c84d.chunk.css"
+  },
+  {
+    "revision": "a3c87166ab4ebb7c4d5c",
+    "url": "/portfolio/static/js/2.8d424c7e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.8d424c7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb4eed8b429102fa263c",
+    "url": "/portfolio/static/js/main.3d8569d2.chunk.js"
+  },
+  {
+    "revision": "b417b3ee81e3e8f9aad5",
+    "url": "/portfolio/static/js/runtime-main.b649af6b.js"
+  }
+]);
