ba42735f76007f68141fe73b7caa5c38acbaf01f
shashankkurakula <shashankkurakula96@gmail.com>
2020-07-06 23:02:24 +0530

    test

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <section className='app'>
+      <h1>Portfolio site under progress</h1>
+      <h3>Please visit back later.</h3>
+      <h6>- Shashank Kurakula</h6>
+    </section>
+  );
+}
+
+export default App;
